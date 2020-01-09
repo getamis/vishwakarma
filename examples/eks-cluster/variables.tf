4896382c76c94634d87df9ef1a232dacf90113f9
kairen <k2r2.bai@gmail.com>
2020-01-09 13:27:46 +0800

    eks: add support for fargate

--- examples/eks-cluster/variables.tf
+++ examples/eks-cluster/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to build network infrastructure"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-2"
 }
 
 variable "key_pair_name" {
