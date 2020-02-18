19be3aa98e59599a0aada48f29e9f1c7fb3a2f1f
kairen <k2r2.bai@gmail.com>
2020-02-18 18:09:28 +0800

    Remove EKS modules and examples

--- modules/aws/eks-worker/data.tf
+++
@@ -1,4 +0,0 @@
-
-data "aws_eks_cluster" "vishwakarma" {
-  name = var.cluster_name
-}
--- modules/aws/eks-worker/resources/userdata.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-
-# Bootstrap and join the cluster
-/etc/eks/bootstrap.sh --b64-cluster-ca '${cluster_auth_base64}' --apiserver-endpoint '${endpoint}' --kubelet-extra-args '${kubelet_extra_args}' '${cluster_name}'
