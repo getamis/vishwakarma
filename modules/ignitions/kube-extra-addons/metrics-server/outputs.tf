2273cb47db7bd5d00a8a8eccb99d14e8c1f5e0c0
Kyle Bai <k2r2.bai@gmail.com>
2020-08-04 16:10:23 +0800

    feat: move etcd and Kubernetes ignition to new repos (#82)
    * feat: move etcd and Kubernetes ignition to new repos
    
    * ci: bump terraform version to 0.12.29

--- modules/ignitions/kube-extra-addons/metrics-server/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "files" {
-  value = [
-    data.ignition_file.metrics_server_components.rendered,
-  ]
-}
-
--- modules/ignitions/kubeconfig/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "files" {
-  value = [
-    data.ignition_file.kubeconfig.rendered,
-  ]
-}
-
-output "content" {
-  value = coalesce(var.content, data.ignition_file.kubeconfig.content[0].content)
-}
