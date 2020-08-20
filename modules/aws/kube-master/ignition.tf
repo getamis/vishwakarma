851d84b9a77b333e1de87a09fa84ff745baf483f
kairen <k2r2.bai@gmail.com>
2020-08-20 11:22:08 +0800

    improvement: update etcd and kubernetes ignition source

--- modules/aws/kube-master/ignition.tf
+++ modules/aws/kube-master/ignition.tf
@@ -12,7 +12,7 @@ resource "random_password" "encryption_secret" {
 }
 
 module "ignition_kubernetes" {
-  source = "git::ssh://git@github.com/getamis/terraform-kubernetes-ignition?ref=v0.1.0"
+  source = "git::ssh://git@github.com/getamis/terraform-ignition-kubernetes?ref=v0.1.0"
 
   binaries              = var.binaries
   containers            = var.containers
