19bc3a7365682face53490604120a347aa8814e8
smalltown <smalltown20110306@gmail.com>
2020-11-10 17:10:08 +0800

    remove unused local variable

--- modules/aws/kube-etcd/variables.tf
+++ modules/aws/kube-etcd/variables.tf
@@ -108,21 +108,6 @@ variable "instance_config" {
   })
 }
 
-locals {
-  instance_config = merge({
-    count              = "1"
-    ec2_type           = "t3.medium"
-    root_volume_size   = "40"
-    data_volume_size   = "100"
-    data_device_name   = "/dev/sdf"
-    data_device_rename = "/dev/nvme1n1"
-    data_path          = "/etcd/data"
-
-    // CoreOS Container Linux stable 2512.3.0
-    image_id = "ami-0c45c2b94700c3e25"
-  }, var.instance_config)
-}
-
 variable "ssh_key" {
   description = "The key name that should be used for the instances."
   type        = string
