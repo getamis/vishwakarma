f5d2080e7ebe01a9de5367ecb36a4d483e87d118
smalltown <smalltown20110306@gmail.com>
2021-06-18 16:19:53 +0800

    remove port 22 from master & worker node

--- modules/aws/kube-master/sg.tf
+++ modules/aws/kube-master/sg.tf
@@ -89,16 +89,6 @@ resource "aws_security_group_rule" "master_ingress_from_lb" {
   to_port   = var.apiserver_secure_port
 }
 
-resource "aws_security_group_rule" "master_ssh" {
-  type              = "ingress"
-  security_group_id = local.master_sg_id
-
-  protocol    = "tcp"
-  cidr_blocks = [data.aws_vpc.master.cidr_block]
-  from_port   = 22
-  to_port     = 22
-}
-
 resource "aws_security_group_rule" "master_ingress_kubelet_secure_from_worker" {
   type              = "ingress"
   security_group_id = local.master_sg_id
