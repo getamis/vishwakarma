21de805da3c7aad75b201742d9dc67efa324afa2
smalltown <smalltown20110306@gmail.com>
2021-06-18 19:12:01 +0800

    remote etcd port 22

--- modules/aws/kube-etcd/sg.tf
+++ modules/aws/kube-etcd/sg.tf
@@ -42,16 +42,6 @@ resource "aws_security_group_rule" "etcd_ingress_from_master" {
   to_port   = local.client_port
 }
 
-resource "aws_security_group_rule" "etcd_ssh" {
-  type              = "ingress"
-  security_group_id = aws_security_group.etcd.id
-
-  protocol    = "tcp"
-  cidr_blocks = [data.aws_vpc.etcd.cidr_block]
-  from_port   = 22
-  to_port     = 22
-}
-
 resource "aws_security_group_rule" "ingress_node_exporter_from_worker" {
   type              = "ingress"
   security_group_id = aws_security_group.etcd.id
