25e916c9ca7764fe070f08984ffa7da94a8046df
Relk <relk@maicoin.com>
2022-01-14 11:25:47 +0800

    feat: upgarde terraform-ignition-kubernetes to v1.4.10

--- modules/aws/kube-master/ignition.tf
+++ modules/aws/kube-master/ignition.tf
@@ -12,7 +12,7 @@ resource "random_password" "encryption_secret" {
 }
 
 module "ignition_kubernetes" {
-  source = "github.com/getamis/terraform-ignition-kubernetes?ref=v1.4.9"
+  source = "github.com/getamis/terraform-ignition-kubernetes?ref=v1.4.10"
 
   binaries              = var.binaries
   containers            = var.containers
