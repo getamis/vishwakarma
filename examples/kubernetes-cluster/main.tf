1ddf7b061326c254403371dc580006dfa8642312
Imo <imo@maicoin.com>
2022-08-11 15:45:05 +0800

    chore : change flatcar ami to 3227.2.1

--- examples/kubernetes-cluster/main.tf
+++ examples/kubernetes-cluster/main.tf
@@ -47,7 +47,7 @@ module "master" {
   etcd_instance_config = {
     count              = 1
     # image_id           = module.os_ami.image_id
-    image_id           = "ami-074e7feabf5e1861f"
+    image_id           = "ami-0b8fef69b7bf66b89"
     ec2_type           = "t3.medium"
     root_volume_size   = 40
     data_volume_size   = 100
@@ -59,7 +59,7 @@ module "master" {
   master_instance_config = {
     count = 1
     # image_id = module.os_ami.image_id
-    image_id = "ami-074e7feabf5e1861f"
+    image_id = "ami-0b8fef69b7bf66b89"
     ec2_type = [
       "t3.medium",
       "t2.medium"
@@ -122,7 +122,7 @@ module "worker_on_demand" {
     name      = "on-demand"
     count     = 1
     max_count = null
-    image_id  = "ami-074e7feabf5e1861f"
+    image_id  = "ami-0b8fef69b7bf66b89"
     ec2_type = [
       "t3.medium",
       "t2.medium"
