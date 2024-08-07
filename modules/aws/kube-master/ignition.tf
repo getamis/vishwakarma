8ea03a65e7545939a633e38b8b2a772cf981e06c
alex.hsu <icewhite@widelab.org>
2024-08-07 11:31:53 +0800

    upgrade to 1.30.3

--- modules/aws/kube-master/ignition.tf
+++ modules/aws/kube-master/ignition.tf
@@ -12,6 +12,7 @@ resource "random_password" "encryption_secret" {
 }
 
 module "ignition_kubernetes" {
+  # source = "github.com/getamis/terraform-ignition-kubernetes//?ref=v1.27.7.1"
   source = "github.com/getamis/terraform-ignition-kubernetes//?ref=v1.27.7.1"
 
   binaries              = var.binaries
