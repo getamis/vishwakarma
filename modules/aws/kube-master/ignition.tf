6639603830cdaf38a0e58a3590e576f9d240ef62
alex.hsu <icewhite@widelab.org>
2024-08-16 18:06:40 +0800

    test - url encode branch name

--- modules/aws/kube-master/ignition.tf
+++ modules/aws/kube-master/ignition.tf
@@ -12,7 +12,7 @@ resource "random_password" "encryption_secret" {
 }
 
 module "ignition_kubernetes" {
-  source = "github.com/getamis/terraform-ignition-kubernetes//?ref=feat/v1.16.1"
+  source = "github.com/getamis/terraform-ignition-kubernetes//?ref=feat%2Fv1.16.1"
 
   binaries              = var.binaries
   containers            = var.containers
