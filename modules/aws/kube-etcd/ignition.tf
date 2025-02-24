8c75f401f9413326d2e5fc35e9f6b673090ae79a
alex.hsu <icewhite@widelab.org>
2025-02-24 18:00:33 +0800

    fix: use terraform-ignition-reinforcements v1.31.1.2

--- modules/aws/kube-etcd/ignition.tf
+++ modules/aws/kube-etcd/ignition.tf
@@ -1,38 +1,38 @@
 module "ignition_docker" {
-  source = "github.com/getamis/terraform-ignition-reinforcements//modules/docker?ref=v1.31.1.1"
+  source = "github.com/getamis/terraform-ignition-reinforcements//modules/docker?ref=v1.31.1.2"
 
   docker_cgroup_driver = "systemd"
   log_level            = var.log_level["docker"]
 }
 
 module "ignition_containerd" {
-  source = "github.com/getamis/terraform-ignition-reinforcements//modules/containerd?ref=v1.31.1.1"
+  source = "github.com/getamis/terraform-ignition-reinforcements//modules/containerd?ref=v1.31.1.2"
 
   log_level = var.log_level["containerd"]
 }
 
 module "ignition_locksmithd" {
-  source = "github.com/getamis/terraform-ignition-reinforcements//modules/locksmithd?ref=v1.31.1.1"
+  source = "github.com/getamis/terraform-ignition-reinforcements//modules/locksmithd?ref=v1.31.1.2"
 
   reboot_strategy = var.reboot_strategy
 }
 
 module "ignition_update_ca_certificates" {
-  source = "github.com/getamis/terraform-ignition-reinforcements//modules/update-ca-certificates?ref=v1.31.1.1"
+  source = "github.com/getamis/terraform-ignition-reinforcements//modules/update-ca-certificates?ref=v1.31.1.2"
 }
 
 module "ignition_node_exporter" {
-  source = "github.com/getamis/terraform-ignition-reinforcements//modules/node-exporter?ref=v1.31.1.1"
+  source = "github.com/getamis/terraform-ignition-reinforcements//modules/node-exporter?ref=v1.31.1.2"
 }
 
 module "ignition_sshd" {
-  source = "github.com/getamis/terraform-ignition-reinforcements//modules/sshd?ref=v1.31.1.1"
+  source = "github.com/getamis/terraform-ignition-reinforcements//modules/sshd?ref=v1.31.1.2"
 
   enable = var.debug_mode
 }
 
 module "ignition_amazon_ec2_net_utils" {
-  source = "github.com/getamis/terraform-ignition-reinforcements//modules/amazon-ec2-net-utils?ref=v1.31.1.1"
+  source = "github.com/getamis/terraform-ignition-reinforcements//modules/amazon-ec2-net-utils?ref=v1.31.1.2"
 
   enabled = var.apply_amazon_ec2_net_utils
 }
