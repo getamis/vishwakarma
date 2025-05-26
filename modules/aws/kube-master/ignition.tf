4496f8020b3322dfcbd32624c48e535f13564255
Che Chia Chang <chechia@maicoin.com>
2025-05-26 14:34:45 +0800

    FEATURE: support capacity_rebalance in asg

--- modules/aws/kube-master/ignition.tf
+++ modules/aws/kube-master/ignition.tf
@@ -12,7 +12,7 @@ resource "random_password" "encryption_secret" {
 }
 
 module "ignition_kubernetes" {
-  source = "github.com/getamis/terraform-ignition-kubernetes//?ref=v1.31.1.1"
+  source                = "github.com/getamis/terraform-ignition-kubernetes//?ref=v1.31.1.1"
   binaries              = var.binaries
   containers            = var.containers
   kubernetes_version    = var.kubernetes_version
